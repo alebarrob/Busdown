95e9f5da32106acc481d2605f343db9466e06714
Alejandro Barrera Robles <abarrobles@gmail.com>
2022-11-17 19:36:54 +0000

    Creating BasicInfoFragment

--- app/src/main/java/barrera/alejandro/busdown/view/MainActivity.kt
+++ app/src/main/java/barrera/alejandro/busdown/view/MainActivity.kt
@@ -1,7 +1,13 @@
 package barrera.alejandro.busdown.view
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.transition.Fade
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import barrera.alejandro.busdown.R
 import barrera.alejandro.busdown.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +17,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            navController.navigate(WelcomeFragmentDirections.actionWelcomeFragmentToBasicInfoFragment())
+        }, 3000)
     }
 }

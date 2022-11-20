2a4ddb8a8b488dc15fbd6f48bf71ed8c5ece971e
Alejandro Barrera Robles <abarrobles@gmail.com>
2022-11-20 18:38:45 +0000

    Creating homeFragment layout

--- app/src/main/java/barrera/alejandro/busdown/view/BasicInfoFragment.kt
+++ app/src/main/java/barrera/alejandro/busdown/view/BasicInfoFragment.kt
@@ -1,6 +1,42 @@
 package barrera.alejandro.busdown.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import barrera.alejandro.busdown.R
+import barrera.alejandro.busdown.databinding.FragmentBasicInfoBinding
 
-class BasicInfoFragment : Fragment(R.layout.fragment_basic_info)
+class BasicInfoFragment : Fragment() {
+    private var _binding: FragmentBasicInfoBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var mainActivity: MainActivity
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBasicInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainActivity = (requireActivity() as MainActivity)
+
+        onClickAcceptButton()
+    }
+
+    private fun onClickAcceptButton() {
+        binding.acceptButton.setOnClickListener {
+            mainActivity.navigateToHomeFragment()
+            mainActivity.bottomNavigationBarIsVisible(true)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/barrera/alejandro/busdown/view/MainActivity.kt
+++ app/src/main/java/barrera/alejandro/busdown/view/MainActivity.kt
@@ -4,28 +4,45 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
+import androidx.navigation.NavDirections
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import barrera.alejandro.busdown.R
 import barrera.alejandro.busdown.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        bottomNavigationView = binding.bottomNavigation
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
         setContentView(binding.root)
 
-        navigateToBasicInfoFragmentAfterTwoSeconds()
+        navigateAfterDelay(WelcomeFragmentDirections.actionWelcomeFragmentToBasicInfoFragment())
     }
 
-    private fun navigateToBasicInfoFragmentAfterTwoSeconds() {
-        Handler(Looper.getMainLooper()).postDelayed({
-            navController.navigate(WelcomeFragmentDirections.actionWelcomeFragmentToBasicInfoFragment())
-        }, 2000)
+    private fun navigateAfterDelay(direction: NavDirections) {
+        Handler(Looper.getMainLooper()).postDelayed(
+            { navController.navigate(direction) },
+            2000
+        )
+    }
+
+    fun navigateToHomeFragment() {
+        navController.navigate(BasicInfoFragmentDirections.actionBasicInfoFragmentToHomeFragment())
+    }
+
+    fun bottomNavigationBarIsVisible(isVisible: Boolean) {
+        binding.bottomNavigation.isVisible = isVisible
     }
 }

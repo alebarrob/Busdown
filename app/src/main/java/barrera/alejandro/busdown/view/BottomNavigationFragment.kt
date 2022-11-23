3033810faebf56b71997629ed777d3fcbbda5dd2
Alejandro Barrera Robles <abarrobles@gmail.com>
2022-11-23 05:23:06 +0000

    SettingsFragment created

--- app/src/main/java/barrera/alejandro/busdown/view/BottomNavigationFragment.kt
+++
@@ -1,6 +0,0 @@
-package barrera.alejandro.busdown.view
-
-import androidx.fragment.app.Fragment
-import barrera.alejandro.busdown.R
-
-class BottomNavigationFragment : Fragment(R.layout.fragment_bottom_navigation)
--- app/src/main/java/barrera/alejandro/busdown/view/ChangeNameFragment.kt
+++
@@ -1,6 +0,0 @@
-package barrera.alejandro.busdown.view
-
-import androidx.fragment.app.Fragment
-import barrera.alejandro.busdown.R
-
-class ChangeNameFragment : Fragment(R.layout.fragment_change_name)
